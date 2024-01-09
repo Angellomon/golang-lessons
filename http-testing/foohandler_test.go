11f51bb393d45a1efac8a26f0b06f2cf22953e14
Angellomon <angelmtzdiaz@gmail.com>
2024-01-09 17:39:44 -0600

    feat(lesson): impl http Testing lesson

---
+++ http-testing/foohandler_test.go
@@ -0,0 +1,64 @@
+package foohandler
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+// ? test with response recording (allows to test programatically)
+func TestHandleGetFooRR(t *testing.T) {
+	rr := httptest.NewRecorder()
+	req, err := http.NewRequest(http.MethodGet, "", nil)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	handleGetFoo(rr, req)
+
+	if rr.Result().StatusCode != http.StatusOK {
+		t.Errorf("expected 200 but got %d", rr.Result().StatusCode)
+	}
+
+	defer rr.Result().Body.Close()
+
+	expected := "FOO"
+	b, err := io.ReadAll(rr.Result().Body)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if string(b) != expected {
+		t.Errorf("expected %s but we go %s", expected, string(b))
+	}
+}
+
+func TestHandleGetFoo(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(handleGetFoo))
+
+	resp, err := http.Get(server.URL)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("excected 200 but got %d", resp.StatusCode)
+	}
+
+	defer resp.Body.Close()
+
+	expected := "FOO"
+
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if string(b) != expected {
+		t.Errorf("expected %s but we got %s", expected, string(b))
+	}
+}
